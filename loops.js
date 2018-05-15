dc1d26c706a27fc63cec98475a49ee55417b27a5
Ainara Saralegui <ainarasaal@gmail.com>
2018-05-15 17:41:36 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,16 +1,31 @@
 function forLoop(array) {
-var i; 
-for (let i= 0; i<25; i++) { 
-   array.push("I am" +"${i}"+ "strange loop")
-}
-console.log("I am" +"${i}"+ "strange loop")
-
-return array
-}
-
-function whileLoop(number) {
-    let countdown = number;
-  while(countdown<25){
-    console.log("done")
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push("I am 1 strange loop.")
+    } else {
+      array.push(`I am ${i} strange loops.`)
+    }
   }
-     }
+
+  return array
+}
+
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(--n)
+  }
+
+  return 'done'
+}
+
+function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+
+  do {
+    array = array.slice(1)
+  } while (array.length > 0 && maybeTrue())
+
+  return 
